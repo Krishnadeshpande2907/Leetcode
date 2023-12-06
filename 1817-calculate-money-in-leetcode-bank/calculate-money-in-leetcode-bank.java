94bec0f4f1e8bb793a197ab56d784949595c2d35
Krishna Deshpande <kd721921@gmail.com>
2023-12-06 23:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (13.57%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,9 +1,9 @@
 class Solution {
     public int totalMoney(int n) {
-        int fullWeeks = n/7;
+        int m = n/7;
         int total = 0;
-        for(int i = 1; i<=fullWeeks; i++)   total += 7*(i+3);
-        for(int i = 7*fullWeeks; i<n; i++)  total += ++fullWeeks;
+        for(int i = 1; i<=m; i++)   total += 7*(i+3);
+        for(int i = 7*m; i<n; i++)  total += ++m;
         return total;
     }
 }
