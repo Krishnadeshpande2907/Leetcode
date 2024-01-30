57c9a66e20e877cb8ae4638abf87f2eecc42d63a
Krishna Deshpande <kd721921@gmail.com>
2024-01-30 12:19:07 +0530

    Time: 10 ms (9.43%) | Memory: 44.7 MB (74.10%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -10,24 +10,16 @@ class MinStack {
     
     public void push(int val) {
         stack.push(val);
-        if (minStack.isEmpty() || val <= minStack.peek()) {
-            minStack.push(val);
-        }
+        if (minStack.isEmpty() || val <= minStack.peek())   minStack.push(val);
     }
     
     public void pop() {
-        if ((int) stack.pop() == (int) minStack.peek()) {
-            minStack.pop();
-        }
+        if ((int) stack.pop() == (int) minStack.peek()) minStack.pop();
     }
     
-    public int top() {
-        return stack.peek();
-    }
+    public int top() {return stack.peek();}
     
-    public int getMin() {
-        return minStack.peek();
-    }
+    public int getMin() {return minStack.peek();}
 }
 
 /**
