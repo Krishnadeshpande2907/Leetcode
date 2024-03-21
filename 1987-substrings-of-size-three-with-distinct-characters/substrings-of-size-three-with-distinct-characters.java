fd263b889aaa5562dee3ed88ffbb9fdb3f35bb0a
Krishna Deshpande <kd721921@gmail.com>
2024-03-21 11:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (99.87%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -3,8 +3,6 @@ class Solution {
         int count = 0;
         char[] arr = s.toCharArray();
         for(int i = 0; i<arr.length-2; i++){
-            // Set<Character> set = new HashSet<Character>();
-            // for(char c : s.substring(i, i+3).toCharArray()) set.add(c);
             if(arr[i] != arr[i+1] && arr[i] != arr[i+2] && arr[i+1] != arr[i+2])   count++;
         }
         return count;
