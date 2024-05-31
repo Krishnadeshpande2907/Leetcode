792f73397ca9e43bb1d8dd36771fc91f20a50189
Krishna Deshpande <kd721921@gmail.com>
2024-05-31 22:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (70.16%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -16,3 +16,12 @@ class MyStack {
     
     public boolean empty() {return st1.isEmpty();}
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
