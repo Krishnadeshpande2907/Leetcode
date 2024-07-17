a689c3e6b8a195dde27e5935f898acf473981f31
Krishna Deshpande <kd721921@gmail.com>
2024-07-17 19:36:09 +0530

    Time: 2 ms (80.64%) | Memory: 45.2 MB (86.45%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-       int reachable = 0;
-       for(int i = 0; i < nums.length; i ++) {
-           if(i > reachable) return false;
-           reachable = Math.max(reachable, i + nums[i]);
-       } 
-       return true;
+        if(nums[0]==0 && nums.length==1)    return true;
+        int canReach = 0;
+        for(int i = 0; i<nums.length; i++){
+            canReach = Math.max(canReach, i+nums[i]);
+            if(canReach>=nums.length-1)    return true;
+            if(i == canReach && nums[canReach]==0)   return false;
+        }
+        return true;
     }
 }
