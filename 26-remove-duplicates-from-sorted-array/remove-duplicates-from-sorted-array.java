9f3b1ec0e151724c22dd23c03074cefa5ae1f8a6
Krishna Deshpande <kd721921@gmail.com>
2024-07-21 11:44:28 +0530

    Time: 1 ms (78.80%) | Memory: 44.4 MB (92.51%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,13 +1,23 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Set<Integer> unique = new HashSet<>();
-        int end = nums.length-1;
-        for(int i = 0; i <= end; i++){
-            if(unique.contains(nums[i]))    swap(i--, end--, nums);
-            else    unique.add(nums[i]);
+        // Set<Integer> unique = new HashSet<>();
+        // int end = nums.length-1;
+        // for(int i = 0; i <= end; i++){
+        //     if(unique.contains(nums[i]))    swap(i--, end--, nums);
+        //     else    unique.add(nums[i]);
+        // }
+        // Arrays.sort(nums);
+        // return unique.size();
+
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
         }
-        Arrays.sort(nums);
-        return unique.size();
+        return j;
+
     }
     private void swap(int a, int b, int[] arr){
         arr[a] = arr[b];
