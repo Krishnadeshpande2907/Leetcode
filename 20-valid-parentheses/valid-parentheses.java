58607750b79c73966c7c56d9de14d6d63e3bd08e
Krishna Deshpande <kd721921@gmail.com>
2024-07-22 18:10:26 +0530

    Time: 1 ms (98.95%) | Memory: 41.2 MB (74.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> rack = new Stack<Character>();
-        for (char bracket : s.toCharArray()) {
-            if (bracket == '(' || bracket == '[' || bracket == '{')   rack.push(bracket);
-            else {
-                if (rack.isEmpty())    return false;
+        Stack<Character> rack = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            char bracket = s.charAt(i);
+            if(bracket == '(' || bracket == '{' || bracket == '[')  rack.push(bracket);
+            else{
+                if(rack.isEmpty())  return false;
                 char top = rack.peek();
-                if ((bracket == ')' && top == '(') || (bracket == ']' && top == '[') || (bracket == '}' && top == '{'))   rack.pop();
+                if((bracket == ')' && top == '(') 
+                || (bracket == '}' && top == '{') 
+                || (bracket == ']' && top == '['))  rack.pop();
                 else    return false;
             }
         }
